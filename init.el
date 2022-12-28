36559aa21e0b50add93359a138015c71e544bb8c
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2022-12-28 10:05:04 -0800

    updated init.el

--- init.el
+++ init.el
@@ -7,15 +7,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-enabled-themes '(leuven-dark))
  '(display-fill-column-indicator t)
  '(fill-column 80)
  '(global-auto-revert-mode t)
  '(global-visual-line-mode t)
  '(org-confirm-babel-evaluate nil)
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 1.4 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-hide-emphasis-markers t)
  '(package-selected-packages
-   '(boxquote citeproc multiple-cursors pdf-tools mixed-pitch org-appear use-package))
+   '(conda corfu boxquote citeproc multiple-cursors pdf-tools mixed-pitch org-appear use-package))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
@@ -24,6 +26,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :height 110 :foundry "GOOG" :slant normal :weight regular :width normal))))
+ '(default ((t (:family "Roboto Mono" :height 120 :foundry "GOOG" :slant normal :weight regular :width normal))))
  '(fixed-pitch ((t (:family "Roboto Mono"))))
- '(variable-pitch ((t (:family "Sans Serif")))))
+ '(variable-pitch ((t (:family "Roboto")))))
+
+;; Customize and set theme
+(setq modus-themes-mode-line '(borderless accented))
+(setq modus-themes-region '(bg-only))
+(setq modus-themes-org-blocks 'gray-background)
+(setq modus-themes-headings
+      '((1 . (variable-pitch 1.5))
+	(2 . (1.3))
+	(agenda-date . (1.3))
+	(agenda-structure . (variable-pitch light 1.8))
+	(t . (1.1))))
+(load-theme 'modus-operandi t)

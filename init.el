5303d05d5e01e9e18631f0f072eb71ef3f0e727a
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2022-12-31 10:35:42 -0800

    updated init.el

--- init.el
+++ init.el
@@ -7,40 +7,29 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("28a34dd458a554d34de989e251dc965e3dc72bace7d096cdc29249d60f395a82" default))
  '(display-fill-column-indicator t)
  '(fill-column 80)
  '(global-auto-revert-mode t)
  '(global-visual-line-mode t)
- '(org-confirm-babel-evaluate nil)
- '(org-format-latex-options
-   '(:foreground default :background default :scale 1.4 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
- '(org-hide-emphasis-markers t)
  '(package-selected-packages
-   '(code-cells conda corfu boxquote citeproc multiple-cursors pdf-tools mixed-pitch org-appear use-package))
- '(python-indent-offset 4)
+   '(code-cells conda corfu citeproc multiple-cursors pdf-tools org-appear use-package))
  '(python-shell-interpreter "ipython3")
  '(python-shell-interpreter-args "--simple-prompt")
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
+;; Customize and set theme
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :height 120 :foundry "GOOG" :slant normal :weight regular :width normal))))
- '(fixed-pitch ((t (:family "Roboto Mono"))))
- '(variable-pitch ((t (:family "Roboto")))))
-
-;; Customize and set theme
-(setq modus-themes-mode-line '(borderless accented))
-(setq modus-themes-region '(bg-only))
-(setq modus-themes-org-blocks 'gray-background)
-(setq modus-themes-headings
-      '((1 . (variable-pitch 1.5))
-	(2 . (1.3))
-	(agenda-date . (1.3))
-	(agenda-structure . (variable-pitch light 1.8))
-	(t . (1.1))))
-(load-theme 'modus-operandi t)
+ '(default ((t (:family "Roboto Mono" :height 110))))
+ '(fixed-pitch ((t (:family "Roboto Mono" :height 110))))
+ '(org-block ((t (:extend t :background "#494949" :family "Roboto Mono" :height 110))))
+ '(org-document-title ((t (:inherit default :foreground "#8CD0D3" :weight bold :height 1.25))))
+ '(org-level-1 ((t (:inherit default :foreground "#DFAF8F" :weight bold :height 1.1))))
+ '(variable-pitch ((t (:family "Baskerville" :height 150)))))
